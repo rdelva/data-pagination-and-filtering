491f9ab4db24946197a0565389aea49de8fb3613
rdelva <roseldelva@gmail.com>
2020-08-16 00:49:44 -0400

    setting up addPagination()

--- js/script.js
+++ js/script.js
@@ -19,14 +19,34 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
-const showPage = ( data, 1 ) => {
-  const studentItem = document.querySelector(".student-item");
+//A list parameter to represent student data that will be passed as an argument when the function is called.
+//A page parameter to represent the page number that will be passed as an argument when the function is called.
+
+const showPage = ( list, page ) => {
+  const itemsPerPage =  9;
+  const startIndex = (page * itemsPerPage) - itemsPerPage;
+  const endIndex = page * itemsPerPage;
+ 
+  const studentList = document.querySelector(".student-list");
+  let html ="";
   for(let i = 0; i < data.length; i++){
-    
-
+    html += `<li class="student-item cf">
+        <div class="student-details">
+          <img class="avatar" src="${data[i].picture.large}" alt="Profile Picture">
+          <h3>${data[i].name.first} ${data[i].name.last} </h3>
+          <span class="email">${data[i].email}</span>
+        </div>
+        <div class="joined-details">
+          <span class="date">Joined ${data[i].registered.date}</span>
+        </div>
+  </li>`;
+   
   }
+ // console.log(html);
+  studentList.insertAdjacentHTML('beforeend', html); 
+  addPagination(startIndex, endIndex);
 
-}
+};// end of showPage
 
 
 
@@ -34,6 +54,14 @@ const showPage = ( data, 1 ) => {
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+
+const addPagination = (start, end) => {
+    
+    for(let i=0; i <= end; i++){
+      console.log(i);
+
+    }
+};
 
 
 
